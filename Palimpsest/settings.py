ed076c29d0ae66dfc7bc6283bf504db03617d927
gcbanevicius <gcbanevicius@gmail.com>
2014-04-01 00:54:54 -0400

    fixing db

--- Palimpsest/settings.py
+++ Palimpsest/settings.py
@@ -17,7 +17,8 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
-BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+#BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = 'staticfiles'
 
 STATICFILES_DIRS = (
