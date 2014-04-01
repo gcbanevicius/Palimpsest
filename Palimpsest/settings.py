f624fe8aabc69456d57ff4c6899bc233d85f1f52
gcbanevicius <gcbanevicius@gmail.com>
2014-04-01 01:04:00 -0400

    working on db

--- Palimpsest/settings.py
+++ Palimpsest/settings.py
@@ -64,7 +64,11 @@ WSGI_APPLICATION = 'Palimpsest.wsgi.application'
 
 # Parse database configuration from $DATABASE_URL
 import dj_database_url
-DATABASES['default'] =  dj_database_url.config()
+#DATABASES['default'] =  dj_database_url.config()
+DATABASES = {
+        'default': dj_database_url.config() #default=os.environ.get('DATABASE_URL'))
+}
+
 
 
 # Database
