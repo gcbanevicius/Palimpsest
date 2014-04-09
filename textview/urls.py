16c531659bcc315de60aa6d3f851dad1bd60431e
Anjalie Field <anjaliefield@nat-oitwireless-inside-vapornet100-b-18091.princeton.edu>
2014-04-09 19:54:51 -0400

    adfgljabdfgljafd

--- textview/urls.py
+++ textview/urls.py
@@ -4,8 +4,9 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from textview import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.index, name='index'),
-    url(r'^two_text', views.two_text, name='two_text')
+    url(r'^', views.index, name='index'),
+    url(r'^two_text/(?P<text_name>.*)$', views.two_text, name='start_text'),
+    url(r'^two_text', views.two_text, name='two_text'), 
 )
 
 urlpatterns += staticfiles_urlpatterns()
--- textview/views.py
+++ textview/views.py
@@ -7,30 +7,28 @@ from textview.models import Text
 
 # Create your views here.
 
-def index(request):
+def index(request, text_name=""):
     # attempt at pulling in whole text file from database!
     temp = loader.get_template('single_text.html')
     gal_war_eng = ""
-    for t in Text.objects.all():
-        gal_war_eng += t.text_field
-        gal_war_eng += '\n'
     c = Context ({
       'name': 'Caesar',
-      'gal_war_eng': Text.objects.all()
+      'gal_war_eng': Text.objects.all(), 
+      'text_url': "single_text"
       })
 
     return HttpResponse(temp.render(c))
 
-def two_text(request):
+def two_text(request, text_name=""):
     # attempt at pulling in whole text file from database!
     temp = loader.get_template('two_text.html')
+
     gal_war_eng = ""
-    for t in Text.objects.all():
-        gal_war_eng += t.text_field
-        gal_war_eng += '\n'
+
     c = Context ({
       'name': 'Caesar',
-      'gal_war_eng': Text.objects.all()
+      'gal_war_eng': Text.objects.all(), 
+      'text_url': "single_text"
       })
 
     return HttpResponse(temp.render(c))
