1b0cfb735f90ea223d75370b345d3db797b3c1b8
Anjalie Field <anjaliefield@nat-oitwireless-inside-vapornet100-b-18091.princeton.edu>
2014-04-09 23:48:53 -0400

    4.9

--- textview/urls.py
+++ textview/urls.py
@@ -6,6 +6,7 @@ from textview import views
 urlpatterns = patterns('',
     url(r'^(?P<text_name>[-\w]+)/$', views.index, name='index'),
     url(r'^two_text/(?P<text_name>[-\w]+)/$', views.two_text, name='two_text'),
+    url(r'^vocab/(?P<text_name>[-\w]+)/$', views.vocab, name='vocab'),
     #url(r'^two_text', views.two_text, name='two_text')
 )
 
--- textview/views.py
+++ textview/views.py
@@ -7,26 +7,35 @@ from textview.models import Text
 
 # Create your views here.
 
+#Default view, displays 1 plain text
 def index(request, text_name=""):
-    # attempt at pulling in whole text file from database!
     temp = loader.get_template('single_text.html')
     gal_war_eng = ""
     c = Context ({
       'text_id': text_name,
-      'gal_war_eng': Text.objects.all(), 
+      'text1': Text.objects.all(), 
       })
 
     return HttpResponse(temp.render(c))
 
+#Corresponds to "translate" button, display two texts side by side
 def two_text(request, text_name=""):
-    # attempt at pulling in whole text file from database!
     temp = loader.get_template('two_text.html')
-
-    gal_war_eng = ""
 
     c = Context ({
       'text_id': text_name,
-      'gal_war_eng': Text.objects.all(), 
+      'text_left': Text.objects.all(), 
+      'text_right': Text.objects.all()
+      })
+
+    return HttpResponse(temp.render(c))
+
+
+def vocab(request, text_name=""):
+    temp = loader.get_template('vocab.html')
+
+    c = Context ({
+      'text_id': text_name,
       })
 
     return HttpResponse(temp.render(c))
